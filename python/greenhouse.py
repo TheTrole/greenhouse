4eb977819624afc82d39be77aeef52d04c4bdde8
AndrijaTrosic <sculpt.innovations@outlook.com>
2024-07-01 16:42:44 +0200

    Project set up and connected with python

--- python/greenhouse.py
+++ python/greenhouse.py
@@ -3,15 +3,18 @@ import random
 import time
 
 # Configure the serial connection (adjust 'COM3' and baudrate as needed)
-ser = serial.Serial('COM3', 9600, timeout=1)
+ser = serial.Serial('COM2', 9600, timeout=1)
 
 try:
     while True:
-        # Generate a random float between 0 and 1
-        random_number = random.random()
+        # Generate a random integer between 0 and 1
+        random_number = random.randint(0, 1)
         
         # Write the random number to the serial port
-        ser.write(f"{random_number}\n".encode('utf-8'))
+        if random_number == 0:
+            ser.write(f"T".encode())
+        else:
+            ser.write(f"F".encode())
         
         # Print the random number to the console (for debugging purposes)
         print(f"Sent: {random_number}")
