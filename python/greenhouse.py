c9fa658ce62c5207da098cd58169214e3428c314
nidza01 <nikola.spasojevic80.19@gmail.com>
2024-07-03 12:15:35 +0200

    Added complete documentation

--- python/greenhouse.py
+++ python/greenhouse.py
@@ -2,21 +2,18 @@ import serial
 import random
 import time
 
-# Configure the serial connection (adjust 'COM3' and baudrate as needed)
+
 ser = serial.Serial('COM2', 9600, timeout=1)
 
 try:
     while True:
-        # Generate a random integer between 0 and 1
         random_number = random.randint(0, 1)
-        
-        # Write the random number to the serial port
+
         if random_number == 0:
             ser.write(f"T".encode())
         else:
             ser.write(f"F".encode())
         
-        # Print the random number to the console (for debugging purposes)
         print(f"Sent: {random_number}")
         
         # Wait for 5 seconds
@@ -26,6 +23,5 @@ except KeyboardInterrupt:
     print("Interrupted by user")
 
 finally:
-    # Close the serial connection
     ser.close()
     print("Serial connection closed")
